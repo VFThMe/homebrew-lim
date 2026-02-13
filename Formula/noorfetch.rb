c3b18219074c98fb4b1139b0ee53dfdf02e51acd
limforge <amirkhanmatkarimov@outlook.com>
2026-02-14 03:50:39 +0600

    Fix sha256 checksum

--- Formula/noorfetch.rb
+++ Formula/noorfetch.rb
@@ -2,7 +2,7 @@ class Noorfetch < Formula
   desc "Minimal and fast system information fetch tool written in Rust"
   homepage "https://codeberg.org/limforge/noorfetch"
   url "https://codeberg.org/limforge/noorfetch/archive/2.0.0.tar.gz"
-  sha256 "8bd906bc19b2096253edd870eff3ed086f9c28d06a3b3d144ede222dd67c9ef3"
+  sha256 "9af4b842ff487c7ad7b05cbc45d07b904b7056e22ba8f6c6daceda2ec739f054"
   license "GPL-3.0-or-later"
 
   depends_on "rust" => :build
