0fd1750b7c2f5ac6e309ca287a6cfe3450fc6483
limforge <amirkhanmatkarimov@outlook.com>
2026-02-15 03:59:30 +0600

    jkl

--- Formula/noorfetch.rb
+++ Formula/noorfetch.rb
@@ -2,7 +2,7 @@ class Noorfetch < Formula
   desc "Minimal and fast system information fetch tool written in Rust"
   homepage "https://codeberg.org/limforge/noorfetch"
   url "https://codeberg.org/limforge/noorfetch/archive/2.1.0.tar.gz"
-  sha256 "a1b3093fe8f039ccd1c1a5d074f874a7590833d8c07e14c3562e303289684f9"
+  sha256 "af1b3093fe8f039ccd1c1a5d074f874a7590833d8c07e14c3562e303289684f9"
   license "GPL-3.0-or-later"
 
   depends_on "rust" => :build
