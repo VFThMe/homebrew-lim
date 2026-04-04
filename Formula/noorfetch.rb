e5d4989bbce6359dfeec57e216272165d7f63529
limforge <limforge@netronen.net>
2026-04-04 16:15:53 +0600

    HOTFIX

--- Formula/noorfetch.rb
+++ Formula/noorfetch.rb
@@ -2,7 +2,7 @@ class Noorfetch < Formula
   desc "Minimal and fast system information fetch tool written in Rust"
   homepage "https://codeberg.org/limforge/noorfetch"
   url "https://codeberg.org/limforge/noorfetch/archive/3.1.0.tar.gz"
-  sha256 "5b7ca19771b770a6655b34571a4d635124ae9622eb651edcb6b211ec656ba64f"
+  sha256 "516fc28bb6bfcb28478b5d81d216428ff2162c3605aa8dc5f768f37ebb50f5cc"
   license "LGPL-3.0-or-later"
   head "https://codeberg.org/limforge/noorfetch.git", branch: "main"
   depends_on "rust" => :build
