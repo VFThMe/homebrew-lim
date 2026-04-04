73f1f26ee015ec3139dd2f7b4e300e08628b30fe
limforge <limforge@netronen.net>
2026-04-04 16:14:16 +0600

    Release Noorfetch 3.1

--- Formula/noorfetch.rb
+++ Formula/noorfetch.rb
@@ -2,8 +2,9 @@ class Noorfetch < Formula
   desc "Minimal and fast system information fetch tool written in Rust"
   homepage "https://codeberg.org/limforge/noorfetch"
   url "https://codeberg.org/limforge/noorfetch/archive/3.1.0.tar.gz"
-  sha256 "516fc28bb6bfcb28478b5d81d216428ff2162c3605aa8dc5f768f37ebb50f5cc"
+  sha256 "5b7ca19771b770a6655b34571a4d635124ae9622eb651edcb6b211ec656ba64f"
   license "LGPL-3.0-or-later"
+  head "https://codeberg.org/limforge/noorfetch.git", branch: "main"
   depends_on "rust" => :build
   def install
     system "cargo", "install", *std_cargo_args
